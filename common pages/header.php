74fff85d9c67e5572434a43e04c4aedc73fe8f5b
iamchaze <virajkale9604@gmail.com>
2023-06-19 18:04:42 +0530

    updated code

--- common pages/header.php
+++ common pages/header.php
@@ -1,197 +1,219 @@
+
 <div class="header sticky-top">
-        <nav class="navbar navbar-expand-md navbar-light">
-            <a class="navbar-brand" href="./index.php">
-                <img src="./img/logo.png" />
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#my-navbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <nav class="navbar navbar-expand-md navbar-light">
+        <a class="navbar-brand" href="./index.php">
+            <img src="./img/logo.png" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#my-navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse justify-content-end" id="my-navbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" data-toggle="modal" data-target="#signup-modal">
-                            <i class="fas fa-user"></i>Signup
-                        </a>
-                    </li>
-                    <div class="nav-vl"></div>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" data-toggle="modal" data-target="#login-modal">
-                            <i class="fas fa-sign-in-alt"></i>Login
-                        </a>
-                    </li>
-                </ul>
+        <div class="collapse navbar-collapse justify-content-end" id="my-navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#signup-modal">
+                        <i class="fas fa-user"></i>Signup
+                    </a>
+                </li>
+                <div class="nav-vl"></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#login-modal">
+                        <i class="fas fa-sign-in-alt"></i>Login
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<div class="modal fade" id="filter-modal" tabindex="-1" role="dialog" aria-labelledby="filter-heading"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="filter-heading">Filters</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-        </nav>
-    </div>
-    <div class="modal fade" id="filter-modal" tabindex="-1" role="dialog" aria-labelledby="filter-heading"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h3 class="modal-title" id="filter-heading">Filters</h3>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
+
+            <div class="modal-body">
+                <h5>Gender</h5>
+                <hr />
+                <div>
+                    <button class="btn btn-outline-dark btn-active">
+                        No Filter
+                    </button>
+                    <button class="btn btn-outline-dark">
+                        <i class="fas fa-venus-mars"></i>Unisex
+                    </button>
+                    <button class="btn btn-outline-dark">
+                        <i class="fas fa-mars"></i>Male
+                    </button>
+                    <button class="btn btn-outline-dark">
+                        <i class="fas fa-venus"></i>Female
                     </button>
                 </div>
+            </div>
 
-                <div class="modal-body">
-                    <h5>Gender</h5>
-                    <hr />
-                    <div>
-                        <button class="btn btn-outline-dark btn-active">
-                            No Filter
-                        </button>
-                        <button class="btn btn-outline-dark">
-                            <i class="fas fa-venus-mars"></i>Unisex
-                        </button>
-                        <button class="btn btn-outline-dark">
-                            <i class="fas fa-mars"></i>Male
-                        </button>
-                        <button class="btn btn-outline-dark">
-                            <i class="fas fa-venus"></i>Female
-                        </button>
+            <div class="modal-footer">
+                <button data-dismiss="modal" class="btn btn-success">Okay</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="signup-modal" tabindex="-1" role="dialog" aria-labelledby="signup-heading"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="signup-heading">Signup with PGLife</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="signup-form" class="form" role="form" method="POST" action="">
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-user"></i>
+                            </span>
+                        </div>
+                        <input type="text" class="form-control" name="full_name" placeholder="Full Name" maxlength="30"
+                            required>
                     </div>
-                </div>
 
-                <div class="modal-footer">
-                    <button data-dismiss="modal" class="btn btn-success">Okay</button>
-                </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-phone-alt"></i>
+                            </span>
+                        </div>
+                        <input type="text" class="form-control" name="phone" placeholder="Phone Number" maxlength="10"
+                            minlength="10" required>
+                    </div>
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-envelope"></i>
+                            </span>
+                        </div>
+                        <input type="email" class="form-control" name="email" placeholder="Email" required>
+                    </div>
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-lock"></i>
+                            </span>
+                        </div>
+                        <input type="password" class="form-control" name="password" placeholder="Password" minlength="6"
+                            required>
+                    </div>
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-university"></i>
+                            </span>
+                        </div>
+                        <input type="text" class="form-control" name="college_name" placeholder="College Name"
+                            maxlength="150" required>
+                    </div>
+
+                    <div class="form-group">
+                        <span>I'm a</span>
+                        <input type="radio" class="ml-3" id="gender-male" name="gender" value="male" /> Male
+                        <label for="gender-male">
+                        </label>
+                        <input type="radio" class="ml-3" id="gender-female" name="gender" value="female" />
+                        <label for="gender-female">
+                            Female
+                        </label>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-block btn-primary">Create Account</button>
+                    </div>
+                </form>
+                <?php
+                    $full_name = $_POST['full_name'];
+                    $phone = $_POST['phone'];
+                    $email = $_POST['email'];
+                    $password = $_POST['password'];
+                    $college_name = $_POST['college_name'];
+                    $gender = $_POST['gender'];
+
+                    $signupQuery = $conn->query("INSERT INTO USERS('EMAIL','PASSWORD','FULL_NAME','PHONE','GENDER','COLLEGE_NAME') VALUES('$email','$password','$full_name','$phone','$gender','$college_name';");
+                    if($signupQuery){
+                        echo "Success!";
+                    }
+            
+
+                ?>
+            </div>
+
+
+
+
+            <div class="modal-footer">
+                <span>Already have an account?
+                    <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#login-modal">Login</a>
+                </span>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="modal fade" id="signup-modal" tabindex="-1" role="dialog" aria-labelledby="signup-heading"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="signup-heading">Signup with PGLife</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form id="signup-form" class="form" role="form">
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fas fa-user"></i>
-                                </span>
-                            </div>
-                            <input type="text" class="form-control" name="full_name" placeholder="Full Name"
-                                maxlength="30" required>
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="login-heading" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="login-heading">Login with PGLife</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                <form id="login-form" class="form" role="form">
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-user"></i>
+                            </span>
                         </div>
+                        <input type="email" class="form-control" name="email" placeholder="Email" required>
+                    </div>
 
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fas fa-phone-alt"></i>
-                                </span>
-                            </div>
-                            <input type="text" class="form-control" name="phone" placeholder="Phone Number"
-                                maxlength="10" minlength="10" required>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-lock"></i>
+                            </span>
                         </div>
+                        <input type="password" class="form-control" name="password" placeholder="Password" minlength="6"
+                            required>
+                    </div>
 
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fas fa-envelope"></i>
-                                </span>
-                            </div>
-                            <input type="email" class="form-control" name="email" placeholder="Email" required>
-                        </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-block btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
 
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fas fa-lock"></i>
-                                </span>
-                            </div>
-                            <input type="password" class="form-control" name="password" placeholder="Password"
-                                minlength="6" required>
-                        </div>
-
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fas fa-university"></i>
-                                </span>
-                            </div>
-                            <input type="text" class="form-control" name="college_name" placeholder="College Name"
-                                maxlength="150" required>
-                        </div>
-
-                        <div class="form-group">
-                            <span>I'm a</span>
-                            <input type="radio" class="ml-3" id="gender-male" name="gender" value="male" /> Male
-                            <label for="gender-male">
-                            </label>
-                            <input type="radio" class="ml-3" id="gender-female" name="gender" value="female" />
-                            <label for="gender-female">
-                                Female
-                            </label>
-                        </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-block btn-primary">Create Account</button>
-                        </div>
-                    </form>
-                </div>
-
-                <div class="modal-footer">
-                    <span>Already have an account?
-                        <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#login-modal">Login</a>
-                    </span>
-                </div>
+            <div class="modal-footer">
+                <span>
+                    <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#signup-modal">Click here</a>
+                    to register a new account
+                </span>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="login-heading"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="login-heading">Login with PGLife</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
+<?php
 
-                <div class="modal-body">
-                    <form id="login-form" class="form" role="form">
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fas fa-user"></i>
-                                </span>
-                            </div>
-                            <input type="email" class="form-control" name="email" placeholder="Email" required>
-                        </div>
-
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fas fa-lock"></i>
-                                </span>
-                            </div>
-                            <input type="password" class="form-control" name="password" placeholder="Password"
-                                minlength="6" required>
-                        </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-block btn-primary">Login</button>
-                        </div>
-                    </form>
-                </div>
-
-                <div class="modal-footer">
-                    <span>
-                        <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#signup-modal">Click here</a>
-                        to register a new account
-                    </span>
-                </div>
-            </div>
-        </div>
-    </div>
+?>
